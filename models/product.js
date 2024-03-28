76337a991eff567601b51c95f7607e4d1149ebd7
mattpetts <matt.petts@gmail.com>
2024-03-28 18:03:57 +0000

    dynamic routes and single product page

--- models/product.js
+++ models/product.js
@@ -19,6 +19,7 @@ module.exports = class Product {
 
     }
     save() {
+        this.id = Math.random().toString;
         getProductsFromFile((products) => {
             products.push(this);
             fs.writeFile(p, JSON.stringify(products), (err)=> {
@@ -28,5 +29,11 @@ module.exports = class Product {
     }
     static fetchAll(callback) {
         getProductsFromFile(callback);
+    }
+    static findById(id, callback) {
+        getProductsFromFile((products) => {
+            const product = products.find(p => p.id === id);
+            callback(product)
+        })
     }
 }
