a8b314abc20c3fa73f0f92cee8a99259bd8c65dc
mattpetts <matt.petts@gmail.com>
2024-03-28 14:52:02 +0000

    add ejs templating engine

--- app.js
+++ app.js
@@ -8,19 +8,23 @@ const bodyParser = require('body-parser');
 // instantiate express
 const app = express();
 
+// Import a templating engine
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
 // require bespoke routes
-const adminRoutes = require('./routes/admin');
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 // middleware
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/admin', adminRoutes);
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 // Catch-all middleware to handle 404s
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+    res.render('404', {pageTitle: 'Page not found!'});
 })
 
 // Create an instance of our app
